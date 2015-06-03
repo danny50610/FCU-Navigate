a99abfc73afa2194a0c483b8c7795c6ed6085c82
Chan, Danny <danny50610@yahoo.com.tw>
2015-06-03 18:17:27 +0800

    在 MarkerSelector ActionBar 新增返回建[WIP]、語音搜尋鍵[WIP]

--- app/src/main/java/iecs/fcu_navigate/MarkerSelectorActivity.java
+++ app/src/main/java/iecs/fcu_navigate/MarkerSelectorActivity.java
@@ -1,9 +1,15 @@
 package iecs.fcu_navigate;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MarkerSelectorActivity extends ActionBarActivity {
@@ -12,6 +18,20 @@ public class MarkerSelectorActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker_selector);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        restoreActionBar();
+        return true;
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 
 }
