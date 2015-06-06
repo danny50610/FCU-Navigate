801397eda9c3d67782ac8e467893031b7fbbdfb3
Chan, Danny <danny50610@yahoo.com.tw>
2015-06-06 18:17:59 +0800

    Clean

--- app/src/main/java/iecs/fcu_navigate/NavigationDrawerFragment.java
+++ app/src/main/java/iecs/fcu_navigate/NavigationDrawerFragment.java
@@ -1,10 +1,8 @@
 package iecs.fcu_navigate;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
