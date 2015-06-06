ec4a9a0175e1e538463bfec64f7d7308171257f9
Chan, Danny <danny50610@yahoo.com.tw>
2015-06-06 18:43:54 +0800

    Clean import

--- app/src/main/java/iecs/fcu_navigate/MarkerSelectorActivity.java
+++ app/src/main/java/iecs/fcu_navigate/MarkerSelectorActivity.java
@@ -1,19 +1,12 @@
 package iecs.fcu_navigate;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.view.MenuItemCompat;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
 public class MarkerSelectorActivity extends ActionBarActivity {
