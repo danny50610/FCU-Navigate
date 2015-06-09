1f25f6236808ecafcf5e4b09563e930568f6c03a
Chan, Danny <danny50610@yahoo.com.tw>
2015-06-09 10:10:29 +0800

    變更地標資料表結構，新增 custom col

--- app/src/main/java/iecs/fcu_navigate/database/MarkerContract.java
+++ app/src/main/java/iecs/fcu_navigate/database/MarkerContract.java
@@ -5,7 +5,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class MarkerContract {
 
@@ -20,10 +26,10 @@ public final class MarkerContract {
             MarkerEntry.COLUMN_NAME_NAME           + TEXT_TYPE    + COMMA_SEP +
             MarkerEntry.COLUMN_NAME_BUILDING_ID    + INTEGER_TYPE + COMMA_SEP +
             MarkerEntry.COLUMN_NAME_FLOOR          + INTEGER_TYPE + COMMA_SEP +
-            MarkerEntry.COLUMN_NAME_CLASSROOM_NAME + TEXT_TYPE    + COMMA_SEP +
             MarkerEntry.COLUMN_NAME_LATITUDE       + REAL_TYPE    + COMMA_SEP +
             MarkerEntry.COLUMN_NAME_LONGITUDE      + REAL_TYPE    + COMMA_SEP +
             MarkerEntry.COLUMN_IMAGE_NAME          + TEXT_TYPE    + COMMA_SEP +
+            MarkerEntry.COLUMN_CUSTOM              + TEXT_TYPE    + COMMA_SEP +
             "FOREIGN KEY(" + MarkerEntry.COLUMN_NAME_CATEGORY_ID + ")" +
                 " REFERENCES " + CategoryContract.CategoryEntry.TABLE_NAME + "(" + CategoryContract.CategoryEntry._ID +  ")" + COMMA_SEP +
             "FOREIGN KEY(" + MarkerEntry.COLUMN_NAME_BUILDING_ID + ")" +
@@ -38,15 +44,15 @@ public final class MarkerContract {
 
     public static void insertDefaultData(SQLiteDatabase db) {
         Item[] markers = new Item[] {
-            new Item("演講廳", "第一國際會議廳", "丘逢甲紀念館",  2, "", 24.178355, 120.648098, ""),
-            new Item("演講廳", "第二國際會議廳", "丘逢甲紀念館",  3, "", 24.178351, 120.647988, ""),
-            new Item("演講廳", "第三國際會議廳", "資訊電機館"  ,  2, "", 24.179299, 120.649739, ""),
-            new Item("演講廳", "第四國際會議廳", "人言大樓"    , -1, "", 0.0, 0.0, ""),
-            new Item("演講廳", "第五國際會議廳", "人言大樓"    , -1, "", 0.0, 0.0, ""),
-            new Item("演講廳", "第六國際會議廳", "人言大樓"    , -1, "", 0.0, 0.0, ""),
-            new Item("演講廳", "第七國際會議廳", "人言大樓"    , -1, "", 0.0, 0.0, ""),
-            new Item("演講廳", "第八國際會議廳", "商學大樓"    ,  8, "", 0.0, 0.0, ""),
-            new Item("演講廳", "第九國際會議廳", "學思樓"      ,  2, "", 0.0, 0.0, ""),
+            new Item("演講廳", "第一國際會議廳", "丘逢甲紀念館",  2, 24.178355, 120.648098, "", new HashMap<String, String>()),
+            new Item("演講廳", "第二國際會議廳", "丘逢甲紀念館",  3, 24.178351, 120.647988, "", new HashMap<String, String>()),
+            new Item("演講廳", "第三國際會議廳", "資訊電機館"  ,  2, 24.179299, 120.649739, "", new HashMap<String, String>()),
+            new Item("演講廳", "第四國際會議廳", "人言大樓"    , -1, 0.0, 0.0, "", new HashMap<String, String>()),
+            new Item("演講廳", "第五國際會議廳", "人言大樓"    , -1, 0.0, 0.0, "", new HashMap<String, String>()),
+            new Item("演講廳", "第六國際會議廳", "人言大樓"    , -1, 0.0, 0.0, "", new HashMap<String, String>()),
+            new Item("演講廳", "第七國際會議廳", "人言大樓"    , -1, 0.0, 0.0, "", new HashMap<String, String>()),
+            new Item("演講廳", "第八國際會議廳", "商學大樓"    ,  8, 0.0, 0.0, "", new HashMap<String, String>()),
+            new Item("演講廳", "第九國際會議廳", "學思樓"      ,  2, 0.0, 0.0, "", new HashMap<String, String>()),
         };
 
         for (Item marker : markers) {
@@ -69,16 +75,17 @@ public final class MarkerContract {
 
         int i = 0;
         Item[] result = new Item[c.getCount()];
+        Type stringStringMap = new TypeToken<Map<String, String>>(){}.getType();
         while (c.moveToNext()) {
             result[i++] = new Item(
                     categoryName,
                     c.getString(2),
                     BuildingContract.getNamebyId(db, c.getLong(3)),
                     c.getInt(4),
-                    c.getString(5),
+                    c.getDouble(5),
                     c.getDouble(6),
-                    c.getDouble(7),
-                    c.getString(8)
+                    c.getString(7),
+                    (Map<String, String>) new Gson().fromJson(c.getString(8), stringStringMap)
             );
         }
 
@@ -91,10 +98,10 @@ public final class MarkerContract {
         public static final String COLUMN_NAME_NAME = "name";
         public static final String COLUMN_NAME_BUILDING_ID = "building_id";
         public static final String COLUMN_NAME_FLOOR = "floor";
-        public static final String COLUMN_NAME_CLASSROOM_NAME = "classroom_name";
         public static final String COLUMN_NAME_LATITUDE = "latitude";
         public static final String COLUMN_NAME_LONGITUDE = "longitude";
         public static final String COLUMN_IMAGE_NAME = "image_name";
+        public static final String COLUMN_CUSTOM = "custom";
     }
 
     public static class Item implements Serializable {
@@ -102,20 +109,20 @@ public final class MarkerContract {
         private String name;
         private String building_name;
         private int floor;
-        private String classroom_name;
         private double latitude;
         private double longitude;
         private String image_name;
+        private Map<String, String> customData;
 
-        public Item(String category_name, String name, String building_name, int floor, String classroom_name, double latitude, double longitude, String image_name) {
+        public Item(String category_name, String name, String building_name, int floor, double latitude, double longitude, String image_name, Map<String, String> customData) {
             this.category_name = category_name;
             this.name = name;
             this.building_name = building_name;
             this.floor = floor;
-            this.classroom_name = classroom_name;
             this.latitude = latitude;
             this.longitude = longitude;
             this.image_name = image_name;
+            this.customData = customData;
         }
 
         public String getCategoryName() {
@@ -134,8 +141,12 @@ public final class MarkerContract {
             return floor;
         }
 
-        public String getClassroomName() {
-            return classroom_name;
+        public Map<String, String> getCustomData() {
+            return customData;
+        }
+
+        public void setCustomData(Map<String, String> customData) {
+            this.customData = customData;
         }
 
         public double getLatitude() {
@@ -157,10 +168,10 @@ public final class MarkerContract {
             values.put(MarkerEntry.COLUMN_NAME_NAME, this.name);
             values.put(MarkerEntry.COLUMN_NAME_BUILDING_ID, BuildingContract.getID(db, this.building_name));
             values.put(MarkerEntry.COLUMN_NAME_FLOOR, this.floor);
-            values.put(MarkerEntry.COLUMN_NAME_CLASSROOM_NAME, this.classroom_name);
             values.put(MarkerEntry.COLUMN_NAME_LATITUDE, this.latitude);
             values.put(MarkerEntry.COLUMN_NAME_LONGITUDE, this.longitude);
             values.put(MarkerEntry.COLUMN_IMAGE_NAME, this.image_name);
+            values.put(MarkerEntry.COLUMN_CUSTOM, new Gson().toJson(this.customData));
 
             return values;
         }
