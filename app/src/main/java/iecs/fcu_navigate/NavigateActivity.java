7f095a441b81dc500a9435408031a656ff0b0c0f
Chan, Danny <danny50610@yahoo.com.tw>
2015-06-10 16:58:23 +0800

    [WIP] 實作 NavigateActivity 選擇地點功能

--- app/src/main/java/iecs/fcu_navigate/NavigateActivity.java
+++ app/src/main/java/iecs/fcu_navigate/NavigateActivity.java
@@ -1,18 +1,51 @@
 package iecs.fcu_navigate;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import iecs.fcu_navigate.database.MarkerContract;
 
 
-public class NavigateActivity extends ActionBarActivity {
+public class NavigateActivity extends ActionBarActivity implements TextView.OnClickListener {
+
+    TextView mOriginName;
+
+    TextView mDestinationName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigate);
+
+        mOriginName = (TextView) findViewById(R.id.textView7);
+        mOriginName.setOnClickListener(this);
+
+        mDestinationName = (TextView) findViewById(R.id.textView8);
+        mDestinationName.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == 1) {
+            Bundle args = data.getExtras();
+            MarkerContract.Item item = (MarkerContract.Item) args.getSerializable("Item");
+
+            if (requestCode == 0) {
+                mOriginName.setText(item.getName());
+            }
+            else if (requestCode == 1) {
+                mDestinationName.setText(item.getName());
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -33,5 +66,13 @@ public class NavigateActivity extends ActionBarActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivityForResult(new Intent().setClass(
+                NavigateActivity.this,
+                MarkerSelectorActivity.class
+        ), (v.getId() == R.id.textView7) ? 0 : 1);
     }
 }
