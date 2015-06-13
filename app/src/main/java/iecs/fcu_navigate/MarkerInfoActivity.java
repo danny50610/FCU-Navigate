76e1ae05aea3a71ea9e6f7a13a66dc343e19d629
Chan, Danny <danny50610@yahoo.com.tw>
2015-06-13 21:19:02 +0800

    改良 MarkerInfo 排版

--- app/src/main/java/iecs/fcu_navigate/MarkerInfoActivity.java
+++ app/src/main/java/iecs/fcu_navigate/MarkerInfoActivity.java
@@ -1,17 +1,29 @@
 package iecs.fcu_navigate;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 import iecs.fcu_navigate.database.MarkerContract;
+import iecs.fcu_navigate.helper.ListViewHelper;
 
 
 public class MarkerInfoActivity extends ActionBarActivity {
+
+    private static final String Item_TITLE = "title";
+
+    private static final String Item_VALUE = "value";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +41,33 @@ public class MarkerInfoActivity extends ActionBarActivity {
     }
 
     private void updateUI(MarkerContract.Item item) {
-        TextView textView = (TextView) findViewById(R.id.textView4);
-        textView.setText(item.getName());
+        ListView infoList = (ListView) findViewById(R.id.listView2);
+
+        ArrayList<Map<String, String>> data = Lists.newArrayList();
+        data.add(ImmutableMap.of(Item_TITLE, "名稱", Item_VALUE, item.getName()));
+        data.add(ImmutableMap.of(Item_TITLE, "位置", Item_VALUE, item.getBuildingName() + " " + getFloorString(item.getFloor())));
+        data.add(ImmutableMap.of(Item_TITLE, "經緯度", Item_VALUE, String.format("%f, %f", item.getLatitude(), item.getLongitude())));
+
+        infoList.setAdapter(new SimpleAdapter(
+                this,
+                data,
+                android.R.layout.simple_list_item_2,
+                new String[]{Item_TITLE, Item_VALUE},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        ));
+        ListViewHelper.updateHeight(infoList);
+    }
+
+    private String getFloorString(int floor) {
+        if (floor > 0) {
+            return floor + "F";
+        }
+        else if (floor < 0) {
+            return "B" + floor * -1;
+        }
+        else {
+            return "";
+        }
     }
 
     @Override
---
+++ app/src/main/java/iecs/fcu_navigate/helper/ListViewHelper.java
@@ -0,0 +1,29 @@
+package iecs.fcu_navigate.helper;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class ListViewHelper {
+
+    //http://stackoverflow.com/a/18863691/4983032
+    public static void updateHeight(ListView myListView) {
+        ListAdapter myListAdapter = myListView.getAdapter();
+        if (myListAdapter == null) {
+            return;
+        }
+        //get listview height
+        int totalHeight = 0;
+        int adapterCount = myListAdapter.getCount();
+        for (int size = 0; size < adapterCount ; size++) {
+            View listItem = myListAdapter.getView(size, null, myListView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        //Change Height of ListView
+        ViewGroup.LayoutParams params = myListView.getLayoutParams();
+        params.height = totalHeight + (myListView.getDividerHeight() * (adapterCount - 1));
+        myListView.setLayoutParams(params);
+    }
+}
